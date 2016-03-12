a8109995761cfeb2ea5d4d458de839fb7725b340
Arceusthepokemon <arceusthepokemon@hotmail.com>
2016-03-12 09:11:32 -0500

    remove the proxys

--- src/main/java/arceus/orefinder/reference/Reference.java
+++ src/main/java/arceus/orefinder/reference/Reference.java
@@ -5,7 +5,4 @@ public class Reference {
     public static final String MOD_NAME = "Ore Finder";
     public static final String VERSION = "0.0.1";
     public static final String GUI = "arceus.orefinder.client.gui.GuiFactory";
-    public static final String CLIENT_PROXY_CLASS = "arceus.orefinder.proxy.ClientProxy";
-    public static final String SERVER_PROXY_CLASS = "arceus.orefinder.proxy.Serverproxy";
-
 }
