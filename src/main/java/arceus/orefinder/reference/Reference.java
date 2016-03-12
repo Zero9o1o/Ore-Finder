4b6b51f0ac8418fcfba0a7c9824d967de7d1c9cb
Zero9o1o <xxphantom.zeroxx@gmail.com>
2016-03-12 11:28:37 -0500

    Cleaned out
    Cleaned out all the files cause the git ignore wasn't set right

--- src/main/java/arceus/orefinder/reference/Reference.java
+++
@@ -1,8 +0,0 @@
-package arceus.orefinder.reference;
-
-public class Reference {
-    public static final String MOD_ID = "orefinder";
-    public static final String MOD_NAME = "Ore Finder";
-    public static final String VERSION = "0.0.1";
-    public static final String GUI = "arceus.orefinder.client.gui.GuiFactory";
-}
