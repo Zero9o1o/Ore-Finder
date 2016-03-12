653cba57ca0254593f31174e4e7a4b91ab988a36
Arceusthepokemon <arceusthepokemon@hotmail.com>
2016-03-12 04:50:56 -0500

    false positive and false negative are now chunk base not click base

--- src/main/java/arceus/orefinder/item/woodenOreFinder.java
+++ src/main/java/arceus/orefinder/item/woodenOreFinder.java
@@ -1,6 +1,7 @@
 package arceus.orefinder.item;
 
 import arceus.orefinder.handler.ConfigurationHandler;
+import arceus.orefinder.item.oreFinderThings.oreFinderChunk;
 import arceus.orefinder.reference.Names;
 import net.minecraft.block.Block;
 import net.minecraft.entity.player.EntityPlayer;
@@ -13,7 +14,7 @@ import java.util.Random;
 public class woodenOreFinder extends arcItem {
 
     private Random arcRandom = new Random();
-
+    private oreFinderChunk oreChunk;
     private boolean ore = false;
 
     public woodenOreFinder() {
@@ -22,6 +23,26 @@ public class woodenOreFinder extends arcItem {
         this.setUnlocalizedName(Names.Items.woodenoreFinder);
         this.setMaxStackSize(1);
         this.setMaxDamage(100);
+
+    }
+
+    private byte rng() {
+
+        if (ConfigurationHandler.do_rng) {
+
+            float rng = arcRandom.nextFloat();
+
+            if (rng <= ConfigurationHandler.wooned_rng2 / 2) {
+
+                return 1;
+
+            } else if (rng <= ConfigurationHandler.wooned_rng2) {
+
+                return 2;
+            }
+        }
+
+        return 0;
 
     }
 
@@ -35,77 +56,91 @@ public class woodenOreFinder extends arcItem {
         } else {
 
             itemstack.damageItem(1, player);
-            if (ConfigurationHandler.do_rng) {
 
-                float rng = arcRandom.nextFloat();
-                System.out.println(rng);
+            if (oreChunk == null) {
 
-                if (rng <= ConfigurationHandler.wooned_rng2 / 2) {
+                oreChunk = new oreFinderChunk(world.getChunkFromBlockCoords(x, z).getChunkCoordIntPair(), rng());
+
+            } else if (oreChunk.var1 == -1) {
+
+                oreChunk.var1 = rng();
+
+            } else if (!oreChunk.chunkXZ.equals(world.getChunkFromBlockCoords(x, z).getChunkCoordIntPair())) {
+
+                oreChunk = new oreFinderChunk(world.getChunkFromBlockCoords(x, z).getChunkCoordIntPair(), rng());
+
+            }
+
+            if (oreChunk.var1 == 1) {
+
+                player.addChatMessage(new ChatComponentTranslation("msg.ore_found.txt"));
+                System.out.println("false positive");
+
+            } else if (oreChunk.var1 == 2) {
+
+                player.addChatMessage(new ChatComponentTranslation("msg.no_ore_found.txt"));
+                System.out.println("false negative");
+
+            } else {
+
+                ore = false;
+                for (byte i = (byte) -ConfigurationHandler.wooned_ore_finder_size; i < ConfigurationHandler.wooned_ore_finder_size; i++) {
+
+                    Block blockX;
+
+                    if (i == 0) {
+
+                        blockX = world.getBlock(x + i, y, z);
+
+                        System.out.println(blockX.getUnlocalizedName());
+
+                        if (blockX.getUnlocalizedName().contains("ore") || blockX.getUnlocalizedName().contains("Ore")) {
+
+                            ore = true;
+                            break;
+                        }
+
+                    } else {
+
+                        blockX = world.getBlock(x + i, y, z);
+                        if (blockX.getUnlocalizedName().contains("ore") || blockX.getUnlocalizedName().contains("Ore")) {
+
+                            ore = true;
+                            break;
+                        }
+
+                        Block blockZ = world.getBlock(x, y, z + i);
+
+                        if (blockZ.getUnlocalizedName().contains("ore") || blockZ.getUnlocalizedName().contains("Ore")) {
+                            ore = true;
+                            break;
+
+                        }
+
+                        Block blockY = world.getBlock(x, y + i, z);
+
+                        if (blockY.getUnlocalizedName().contains("ore") || blockY.getUnlocalizedName().contains("Ore")) {
+                            ore = true;
+                            break;
+
+                        }
+                    }
+                }
+
+                if (ore) {
 
                     player.addChatMessage(new ChatComponentTranslation("msg.ore_found.txt"));
                     return true;
 
-                }
-
-                if (rng <= ConfigurationHandler.wooned_rng2 ) {
+                } else {
 
                     player.addChatMessage(new ChatComponentTranslation("msg.no_ore_found.txt"));
                     return true;
 
                 }
             }
-
-            ore = false;
-            for (byte i = (byte) -ConfigurationHandler.wooned_ore_finder_size; i < ConfigurationHandler.wooned_ore_finder_size; i++) {
-
-                Block blockX;
-
-                if (i == 0) {
-
-                    blockX = world.getBlock(x + i, y, z);
-
-                    System.out.println(blockX.getUnlocalizedName());
-
-                    if (blockX.getUnlocalizedName().contains("ore") || blockX.getUnlocalizedName().contains("Ore")) {
-
-                        ore = true;
-                    }
-
-                } else {
-
-                    blockX = world.getBlock(x + i, y, z);
-                    if (blockX.getUnlocalizedName().contains("ore") || blockX.getUnlocalizedName().contains("Ore")) {
-
-                        ore = true;
-                    }
-
-                    Block blockZ = world.getBlock(x, y, z + i);
-
-                    if (blockZ.getUnlocalizedName().contains("ore") || blockZ.getUnlocalizedName().contains("Ore")) {
-                        ore = true;
-
-                    }
-
-                    Block blockY = world.getBlock(x, y + i, z);
-
-                    if (blockY.getUnlocalizedName().contains("ore") || blockY.getUnlocalizedName().contains("Ore")) {
-                        ore = true;
-
-                    }
-                }
-            }
-
-            if (ore) {
-
-                player.addChatMessage(new ChatComponentTranslation("msg.ore_found.txt"));
-                return true;
-
-            } else {
-
-                player.addChatMessage(new ChatComponentTranslation("msg.no_ore_found.txt"));
-                return true;
-
-            }
         }
+
+        return false;
     }
 }
